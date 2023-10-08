2e0cf3fca8655be54772ac6b6b22347178e621e3
haourgot123 <haourgot123@gmail.com>
2023-10-09 03:41:33 +0700

    update

---
+++ 6.SOURCEANDHEADER/app/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include "config.hpp"
+
+int main()
+{
+    cout << project_name << endl;
+    cout << project_version << endl;
+    cout << project_major << endl;
+    cout << project_minor << endl;
+    cout << project_patch << endl;
+}
